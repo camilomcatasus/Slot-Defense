256b41e39caf1079a4810aecde064bff34bee443
Camilo Catasus <camilomcatasus@gmail.com>
2021-09-26 08:01:12 -0400

    Added More Images, Changing the theme works (kind of)

--- Assets/src/GameManager.cs
+++ Assets/src/GameManager.cs
@@ -9,12 +9,22 @@ public class GameManager : MonoBehaviour
 
     AudioManager sounds;
 
+    public List<Sprite> backgrounds;
+    public List<Sprite> slots;
+    public List<GameObject> enemy;
+
+    public GameObject background;
+
     // Start is called before the first frame update
     void Start()
     {
         sounds = FindObjectOfType<AudioManager>();
         sounds.Play(music);
     }
+    public void SwitchSetting(int index)
+    {
+        background.GetComponent<SpriteRenderer>().sprite = backgrounds[index];
+    }    
 
     // Update is called once per frame
     void Update()
