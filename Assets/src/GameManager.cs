0f8ebfd18494555df3c73da3c7648e1685846ea6
Camilo Catasus <camilomcatasus@gmail.com>
2021-09-26 08:52:33 -0400

    Almost finished change theme functionality.

--- Assets/src/GameManager.cs
+++ Assets/src/GameManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-
+    public Spawner spawner;
     public string music;
-
+   // public int index = 0;
     AudioManager sounds;
 
     public List<Sprite> backgrounds;
@@ -23,6 +23,8 @@ public class GameManager : MonoBehaviour
     }
     public void SwitchSetting(int index)
     {
+        //this.index = index;
+        spawner.enemy[0] = enemy[index];
         background.GetComponent<SpriteRenderer>().sprite = backgrounds[index];
     }    
 
